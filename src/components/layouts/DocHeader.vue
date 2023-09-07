dd439bc1169d08b50fba5d1c8703f7e92db6f118
softcm <softcm@yeah.net>
2023-09-07 13:56:34 +0800

    index

--- src/components/layouts/DocHeader.vue
+++ src/components/layouts/DocHeader.vue
@@ -14,10 +14,6 @@ const { showSide, isMobile } = storeToRefs(appStore);
 
 const about = () => {
   ElMessageBox.alert(
-    `
-  本项目是专为 <a href="https://github.com/l15y/wenda" target="_blank">闻达</a> 设计的webui。<br>
-  本项目git地址： <a href="https://github.com/AlanLee1996/wenda-webui" target="_blank">wenda-webui</a>
-  `,
     "关于本项目",
     {
       dangerouslyUseHTMLString: true,
